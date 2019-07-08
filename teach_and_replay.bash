05dd62b73b8df1d6bd29b69c85a86454f77b8e77
Ryuichi Ueda <ryuichiueda@gmail.com>
2019-07-08 17:26:55 +0900

    Add an example map

--- teach_and_replay.bash
+++ teach_and_replay.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+### do SLAM ###
 roslaunch raspimouse_cartographer slam.launch
 
+### choose the max size trajectory file ###
+# please install gawk with `sudo apt install gawk`
+ls -l /tmp/trajectory_*.tsv |
+sort -k5,5n                 |
+tail -n 1                   |
+gawk '{a=$NF;gsub(/[_0-9]*/,"",a);print $NF,a}'      |
+xargs cp
+
+### do replay ###
 roslaunch raspimouse_map_based_teach_and_replay replay.launch
 
